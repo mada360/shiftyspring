0199c0845baf229c307c006c03dddc2959f7bbb1
Adam Worley <worley9@hotmail.co.uk>
2017-02-27 16:57:15 +0000

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+mvn clean package
 
 sudo docker build --tag=shiftyspring .
 
-sudo docker run -it -p 8080:8080 shiftyspring
+sudo docker-compose up
