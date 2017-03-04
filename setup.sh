8b9418a869ddced3f1dbea248344bd39d3bd76ea
mada360 <worley9@hotmail.co.uk>
2017-03-04 09:41:42 +0000

    Removed need for sudo in script to allow it to run on windows

--- setup.sh
+++ setup.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 mvn clean package
 
-sudo docker build --tag=shiftyspring .
+docker build --tag=shiftyspring .
 
-sudo docker-compose up
+docker-compose up
